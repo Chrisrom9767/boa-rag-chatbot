629ef68f3ed2ef8473b45a9ed441b931512f09da
Chrisrom9767 <160159879+Chrisrom9767@users.noreply.github.com>
2025-10-29 15:23:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from typing import List, Tuple
 
 import streamlit as st
 import torch
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 import groq
 
 # ======================================================================
